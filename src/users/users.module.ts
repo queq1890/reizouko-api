79299c1c18dbfc1826c781e9ef337520da7dc2dc
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-04-27 11:54:27 +0700

    Remove users controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersResolver } from './users.resolver';
 
 @Module({
-  controllers: [UsersController],
   providers: [UsersService, PrismaService, UsersResolver],
 })
 export class UsersModule {}
