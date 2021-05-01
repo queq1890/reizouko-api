e08c6fabe6822b6322a42ffa2d569a97802e8930
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-05-01 17:49:30 +0700

    Add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
-    UsersModule,
+    ConfigModule.forRoot({}),
     GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    UsersModule,
   ],
 })
 export class AppModule {}
