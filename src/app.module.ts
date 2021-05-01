9f323d6e27944a0b7b33706b497f8b99368b0a4f
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-05-01 22:25:49 +0700

    Add GqlAuthGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthzModule } from './authz/authz.module';
 @Module({
   imports: [
     ConfigModule.forRoot({}),
-    GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
     UsersModule,
+    AuthzModule,
   ],
 })
 export class AppModule {}
